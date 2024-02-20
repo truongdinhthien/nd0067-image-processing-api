cc14cab904074319fa2c4b5feb84cfa9d8055275
TruongThien <truongdinhthien260599@gmail.com>
2024-02-20 21:51:37 +0700

    chore: change file utils to folder

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -2,7 +2,6 @@ import fs from 'fs/promises';
 
 export const checkFileExists = async (filePath: string) => {
   try {
-    console.log(filePath);
     await fs.access(filePath, fs.constants.F_OK);
     return true;
   } catch (err) {
