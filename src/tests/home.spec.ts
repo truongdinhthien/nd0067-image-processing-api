95cb79c8e6c0b661c4fbc1ed7a5070508df7ff8e
TruongThien <truongdinhthien260599@gmail.com>
2024-02-26 02:44:59 +0700

    chore: rewrite the home test suite

--- src/tests/home.spec.ts
+++ src/tests/home.spec.ts
@@ -1,14 +1,18 @@
 import request from './helpers/mock-request';
 
-describe('Test home endpoint responses', () => {
-  it('should return status code 200', async () => {
-    const response = await request.get('/api');
-    expect(response.status).toBe(200);
+describe('Test home API', () => {
+  describe('GET /', () => {
+    it('should redirect to the /api route', async () => {
+      const response = await request.get('/');
+      expect(response.status).toBe(302);
+      expect(response.header.location).toBe('/api');
+    });
   });
 
-  it('should redirect from / to the /api route', async () => {
-    const response = await request.get('/');
-    expect(response.status).toBe(302);
-    expect(response.header.location).toBe('/api');
+  describe('GET /api', () => {
+    it('should return status code 200', async () => {
+      const response = await request.get('/api');
+      expect(response.status).toBe(200);
+    });
   });
 });
