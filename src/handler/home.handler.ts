63d63103141950e8c38b862ea8cc8e3b9fde4de5
TruongThien <truongdinhthien260599@gmail.com>
2024-02-26 02:44:40 +0700

    feat: render the README file to homepage

--- src/handler/home.handler.ts
+++ src/handler/home.handler.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
+import path from 'path';
+import fs from 'fs/promises';
+import marked from 'marked';
 
 class HomeHandler {
-  welcome(_: Request, res: Response) {
-    res.status(200).send('API route is working!');
+  async welcome(_: Request, res: Response) {
+    const file = await fs.readFile(path.resolve('./README.md'));
+    res.status(200).send(marked.parse(file.toString()));
   }
 }
 
